7ebe1d9fac76ba290cead77523105ebc405fa64f
marco tugnolo <marcotugnolo.biz@gmail.com>
2024-01-22 20:50:53 +0100

    ,

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      @letters << ('A'..'Z').to_a.sample
+    end
+  end
+
+  def score
+    raise
+  end
+end
